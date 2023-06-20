acbafe3e6ea1d3886555e86ff7d8966bf2959475
HYUNWOO CHEON <milcamboo@gmail.com>
2023-06-20 20:33:35 +0900

    hello world in c

---
+++ myproject/printfscanf.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void){
+    /* int */
+    int age = 12;
+    printf("%d\n", age);
+    age = 13;
+    printf("%d\n", age);
+
+    /* float */
+
+    float f = 45.23f;
+    printf("%.1f\n", f);
+    double d = 4.211113f;
+    printf("%.1f\n", d);
+
+    /* constant number */
+    const int YEAR = 2999;
+    printf("태어난 연도:%d\t", YEAR);
+    return 0;
+}
