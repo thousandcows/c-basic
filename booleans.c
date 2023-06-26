76c63624bb86ec63c600a3463c6c54f8b1e720cd
HYUNWOO CHEON <milcamboo@gmail.com>
2023-06-26 21:52:59 +0900

    feat: boolean values are returned as integers

---
+++ booleans.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+    // boolean values are returned as integers:
+    bool isProgrammingFun = true;
+    bool isFishTasty = false;
+
+    printf("%d\n", isProgrammingFun);
+    printf("%d\n", isFishTasty);
+
+    printf("%d\n", 1 > 8);
+}
+
+
