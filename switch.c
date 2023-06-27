f6c0fc54567e9fd65c59ae881281b443f82a25d1
HYUNWOO CHEON <milcamboo@gmail.com>
2023-06-27 19:53:22 +0900

    feat: switch statement

---
+++ switch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int say = 4;
+
+    switch (say) {
+        case 1:
+            printf("Hello world");
+        case 4:
+            printf("Correct");
+            break;
+        default:
+            printf("hahahahaah");
+    }
+}
