51ed846577765dbfd0d4a75e5933277cc7577c75
HYUNWOO CHEON <milcamboo@gmail.com>
2023-07-02 19:34:56 +0900

    feat: basic math function

---
+++ math.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void) {
+    printf("%.2f\n", sqrt(16));
+    printf("%.2f\n", ceil(1.4));
+    printf("%.2f\n", floor(1.4));
+    printf("%.1f", pow(4, 3));
+}
